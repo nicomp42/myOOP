fc517cded188a76459fd5ab44ffb854e9e4442d7
Owner <Owner@DESKTOP-P6S3L85.wifi.uc.edu>
2024-02-29 13:34:35 -0500

    Commit

---
+++ InClass_20240227/vehiclePackage/jeepClass.py
@@ -0,0 +1,12 @@
+#jeepClass.py
+
+from vehiclePackage.Car import Car
+
+class Jeep(Car):
+    def __init__(self, type, make, model, color):
+        self.color = color;
+    
+    
+        
+        
+    
